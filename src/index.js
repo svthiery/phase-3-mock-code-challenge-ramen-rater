e5b6458c566e9ec52fb91f162116ce53e3400304
Sarah Thiery <sarahvthiery@gmail.com>
2021-01-20 09:42:11 -0500

    Add fetch for menu images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // write your code here
+const ramenMenu = document.querySelector('#ramen-menu')
+console.log(ramenMenu)
+
+fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(ramensArray => {
+    ramensArray.forEach(ramenObj => {
+        newRamenImg(ramenObj)
+    })
+  });
+
+   const newRamenImg = ramen => {
+      const ramenImg = document.createElement('img')
+      ramenImg.src = ramen.image
+      ramenImg.dataset.id = ramen.id
+      ramenMenu.append(ramenImg)
+   }
