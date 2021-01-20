4a74c450bb0ae64a4dd5e22975ccc432e1a2e030
Sarah Thiery <sarahvthiery@gmail.com>
2021-01-20 10:10:15 -0500

    Add event listener to load ramen details to detail div

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 // write your code here
 const ramenMenu = document.querySelector('#ramen-menu')
-console.log(ramenMenu)
+const detailDiv = document.querySelector('#ramen-detail')
+const detailImg = document.querySelector('#ramen-detail img')
+const detailName = document.querySelector('#ramen-detail h2')
+const detailRes = document.querySelector('#ramen-detail h3')
+const ratingForm = document.querySelector('#ramen-rating')
+const rating = document.querySelector('#rating')
+const comment = document.querySelector('#comment')
+console.log(rating)
+console.log(comment)
+
+
 
 fetch('http://localhost:3000/ramens')
-  .then(response => response.json())
-  .then(ramensArray => {
-    ramensArray.forEach(ramenObj => {
-        newRamenImg(ramenObj)
-    })
-  });
+    .then(response => response.json())
+    .then(ramensArray => {
+        ramensArray.forEach(ramenObj => {
+            newRamenImg(ramenObj)
+        })
+    });
 
-   const newRamenImg = ramen => {
-      const ramenImg = document.createElement('img')
-      ramenImg.src = ramen.image
-      ramenImg.dataset.id = ramen.id
-      ramenMenu.append(ramenImg)
-   }
+const newRamenImg = ramen => {
+    const ramenImg = document.createElement('img')
+    ramenImg.src = ramen.image
+    ramenImg.dataset.id = ramen.id
+
+    ramenImg.addEventListener("click", event => {
+         detailImg.src = event.target.src
+         detailName.textContent = ramen.name
+         detailRes.textContent = ramen.restaurant
+         const ratingStr = parseInt(ramen.rating)
+         console.log(ratingStr)
+         rating.value = ratingStr
+         
+         comment.textContent = ramen.comment
+
+    })
+
+    ramenMenu.append(ramenImg)
+}
